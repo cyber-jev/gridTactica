20e65bc9281ce1cc3278cec80d907122842b93ef
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-06-20 22:39:58 +0100

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import homeImg from "./assets/gridtactica-background.jpeg";
 import GameBoard from "./components/game.component";
 
 function App() {
   return (
-    <div
-      className="App h-[100dvh] h-[100vh] w-full bg-cover bg-center shadow-xl flex  "
-      style={{ backgroundImage: `url(${homeImg})` }}
-    >
-      <GameBoard />
-    </div>
+    // <div
+    //   className="App h-[100dvh] h-[100vh] w-full bg-cover bg-center shadow-xl flex  "
+    //   style={{ backgroundImage: `url(${homeImg})` }}
+    // >
+    //   <GameBoard />
+    // </div>
+    <BrowserRouter>
+      <div
+        className="App h-[100dvh] h-[100vh] w-full bg-cover bg-center shadow-xl flex  "
+        style={{ backgroundImage: `url(${homeImg})` }}
+      >
+        <Route path="/" component={GameBoard} />
+      </div>
+    </BrowserRouter>
   );
 }
 
